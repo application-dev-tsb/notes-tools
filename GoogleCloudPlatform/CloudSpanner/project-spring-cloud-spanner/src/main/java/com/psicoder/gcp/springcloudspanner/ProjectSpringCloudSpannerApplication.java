6b75322cbd71d92a60acd7f1548bc51f117b3080
LM Bibera <lyndon.bibera@boltoninternational.com>
2017-11-08 10:09:13 +0800

    generated spring template

---
+++ GoogleCloudPlatform/CloudSpanner/project-spring-cloud-spanner/src/main/java/com/psicoder/gcp/springcloudspanner/ProjectSpringCloudSpannerApplication.java
@@ -0,0 +1,12 @@
+package com.psicoder.gcp.springcloudspanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringCloudSpannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringCloudSpannerApplication.class, args);
+	}
+}
